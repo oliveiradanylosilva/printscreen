7b9c5bd79c1ac13f77bd60f4c92caa471f9a4c47
Danylo Oliveira <oliveira.danylo@yahoo.com.br>
2025-06-17 23:09:00 -0300

    Ajuste de mensagem de conclusão

--- PrintSrc.py
+++ PrintSrc.py
@@ -10,7 +10,8 @@ O Programa irá criar uma pasta com o dia atual, acessar 3 Urls, printar a tela 
 '''
 
 data_atual = str(date.today()) #cria a data atual e transforma em string para ser usada no nome da pasta.
-#data_atual = str(data_atual)
+
+#Seleção de período de execução
 
 def selecionar_horario():
     while True:
@@ -30,26 +31,21 @@ def selecionar_horario():
     return escolha
 
 periodo = selecionar_horario()
-diretorio = (f"C:\\Users\\olive\\Pictures\\{data_atual}-{periodo}\\") 
+diretorio = (f"C:\\{data_atual}-{periodo}\\") 
+#diretorio = (f"C:\\Users\\seu_usuario\\Pictures\\{data_atual}-{periodo}\\") 
+
 
 os.makedirs(diretorio,exist_ok=True) # Cria o diretório com o nome da pasta.'
 
-        
-
-#os.mkdir("C:\\Users\\olive\\Pictures\\"+data_atual) # Cria o diretório com o nome da pasta.
-
-
-#definir os links que desejam que sejam printados.
+       
+#Definir os links que desejam que sejam printados.
 
 link_1  = "https://www.globo.com"
 link_2 =  'https://www.youtube.com.br'
 link_3 =  'https://www.uol.com.br'
 
-#Defina o diretório onde serão salvos 
-
 #diretorio = (f"C:\\Users\\olive\\Pictures\\{data_atual}\\") 
 
-#pyautogui.sleep = 0.4
 pyautogui.hotkey("win","r")
 pyautogui.write("chrome --start-maximized")
 pyautogui.press("enter")
@@ -59,7 +55,7 @@ time.sleep(1)
 pyautogui.write(link_1)
 pyautogui.press("enter")
 time.sleep(5)
-#pyautogui.click(x=1318, y=35) [v1.0]-Remoção do clique de maximixar para solução de start maaximized na linha 31. para uso em demais de resoluções de tela.
+
 pyautogui.sleep(3)
 pyautogui.screenshot(f"{diretorio}PrintGlobo-{data_atual}.png")
 time.sleep(1)
@@ -80,18 +76,7 @@ pyautogui.screenshot(f"{diretorio}PrintUol-{data_atual}.png")
 time.sleep(2)
 
 #CloseChrome
-pyautogui.hotkey("alt","F4") # [v1.0] - remoçao do click para execução em demais resoluções.
+pyautogui.hotkey("alt","F4") 
 
-#pyautogui.click(x=1880, y=29)
-
-
-#1 - Abrir o Chrome - ok
-#2 - Digitar as Urls - ok 
-#3 - Printar cada umas das Urls - ok
-#4 - Salvar em uma pasta com o dia atual do print - ok 
-#5 - possibilidade de uma validação do horário de execução, visto que será executado duas vezes ao dia inicio e fim.OK
-#6 - validação de except quando a pasta já existir, com uma condição de escolha para  sobrepor ou deletar a pasta já existente. OK utilizado oks.mkdirs, ignorando pasta existente.
-
-#To Do - 06062025
-#3 transformar em executável. 
+print(f'Execução concluída com sucesso.Prints salvos no diretório: {diretorio}')
 
